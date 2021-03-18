5765b8af24c43ecd39210b7df38927ede3f434b7
Andy Liu <andyyliu1@gmail.com>
2021-03-18 12:07:02 -0500

    updated params

--- app/src/components/AirportInfo.js
+++ app/src/components/AirportInfo.js
@@ -1,11 +1,16 @@
 import React, {useState} from 'react';
 import './AirportInfo.css';
-import Places from "./Places";
+import Flights from "./Flights";
 
 function AirportInfo() { 
-    const [places,setPlaces] = useState([])
-    const [query,setQuery] = useState("")
+    const [flights,setFlights] = useState([])
     const [showPlaces,setShowPlaces] = useState(false)
+    const [currency,setCurrency] = useState("")
+    const [from,setFrom] = useState("")
+    const [to,setTo] = useState("")
+    const [inbound,setInbound] = useState("")
+    const [outbound,setOutbound] = useState("")
+
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -13,29 +18,38 @@ function AirportInfo() {
             const reqOptions = {
                 method: 'GET',
                 headers: {
-                    "x-rapidapi-key": `${process.env.REACT_APP_API_KEY}`,
+                    "x-rapidapi-key": "359296051emshb0fc111a532c1adp17946ejsn7d7cec06afca",
                     "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
                     "useQueryString": true
                 }
             }
-            let response = await fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/v1.0/US/USD/en-US/?" + new URLSearchParams({query: query}), reqOptions)
+            let trueOutbound = (outbound ? "/" + outbound : "")
+            let endpoint = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/" + currency + "/en-US/" + from + "/" + to + "/" + inbound + trueOutbound
+            let response = await fetch(endpoint, reqOptions)
             response = await response.json()
-            console.log(response.Places)
-            setPlaces(response.Places)
+            console.log(response.Quotes)
+            setFlights(response.Quotes)
         }
         fetchMyAPI()
         setShowPlaces(true)
-        setQuery("")
     }
 
     return(
         <div className="airportinfo">
            <form onSubmit={handleSubmit}>
-                <label htmlFor="queryInput">Origin Place:</label>
-                <input id="queryInput" value={query} onChange={e => setQuery(e.target.value)} required/>
+                <label htmlFor="queryInput">Currency:</label>
+                <input id="queryInput" value={currency} onChange={e => setCurrency(e.target.value)} required/>
+                <label htmlFor="queryInput">Origin:</label>
+                <input id="queryInput" value={from} onChange={e => setFrom(e.target.value)} required/>
+                <label htmlFor="queryInput">Destination:</label>
+                <input id="queryInput" value={to} onChange={e => setTo(e.target.value)} required/>
+                <label htmlFor="queryInput">Start Date:</label>
+                <input id="queryInput" value={inbound} onChange={e => setInbound(e.target.value)} required/>
+                <label htmlFor="queryInput">End Date:</label>
+                <input id="queryInput" value={outbound} onChange={e => setOutbound(e.target.value)} />
                 <button className="search">Submit</button>
            </form>
-           { showPlaces ? <Places places={places}></Places> : <></>}
+           { showPlaces ? <Flights flights={flights}></Flights> : <></>}
         </div>
     )
 }
---
+++ app/src/components/Flights.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Flights.css';
+
+function Flights(props) { 
+    return(
+        <div className="flights">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Quote ID</th>
+                        <th>Price</th>
+                        <th>Direct Flight</th>
+                        {/* <th>Region ID</th>
+                        <th>City ID</th>
+                        <th>Country Name</th> */}
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.flights.map(flight => {
+                        return (<tr id={flight.QuoteId}>
+                            <th>{flight.QuoteId}</th>
+                            <th>{flight.MinPrice}</th>
+                            <th>{flight.Direct ? "Yes" : "No"}</th>
+                            <th>{flight.CountryId}</th>
+                            <th>{flight.RegionId}</th>
+                            <th>{flight.CityId}</th>
+                            <th>{flight.CountryName}</th>
+                        </tr>);
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Flights;
