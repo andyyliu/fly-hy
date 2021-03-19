b5dacb96ac2a358a992f8919c1ec549e7f349919
Andy Liu <andyyliu1@gmail.com>
2021-03-19 02:25:49 -0500

    added logo

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Header.css';
+import logo from './plane.png';
 
-function Header(props) { 
-    return(
+function Header(props) {
+    return (
         <div className="header">
+            <img id="logo" src={logo} alt="logo"></img>
             {props.title}
         </div>
     )
